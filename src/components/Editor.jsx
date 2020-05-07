9aa5573aa08325421d178def51ca70e6fd14bd11
Ayush Ojha <ayushojha@gmail.com>
2020-05-07 22:42:25 +0530

    changes applied as suggested

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,54 +1,67 @@
-import React from 'react';
-import { render } from 'react-dom';
-import MonacoEditor from 'react-monaco-editor';
+import React from "react";
+import { render } from "react-dom";
+import MonacoEditor from "react-monaco-editor";
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: '',
-      defaultValue: 
-      `(async function respond (inputText) {
+      code: `
+      /**
+       * Enter your every-time code here.
+       * This function will run every time when user performs some action.
+       * You can remove "async" keyword if you don't have any await function inside this function
+       */
+
+      (async function respond (inputText) {
         var language = 'French';
         var transalatedOutPut = await axios.get('https://reqres.in/api/users/2');
         
-        return  "Oh in french that is" + transalatedOutPut.data.data.email;
-    })`
-    }
+        return  "Oh in french that is " + transalatedOutPut.data.data.email;
+    })`,
+    };
   }
-   editorDidMount = (editor, monaco) => {
-    console.log('editorDidMount', editor);
-    localStorage.setItem('code', '');
+  editorDidMount = (editor, monaco) => {
+    console.log("editorDidMount", editor);
+    localStorage.setItem("code", this.state.code);
     editor.focus();
-  }
+  };
   onChange = (newValue, e) => {
-    console.log('onChange', newValue, e);
-    localStorage.setItem('code', newValue);
-  }
+    console.log("onChange", newValue, e);
+    this.setState({
+      code: newValue,
+    });
+    // localStorage.setItem("code", newValue);
+  };
   onSubmit = (editor, monaco) => {
     const model = editor.getModel();
     const value = model.getValue();
     alert(value);
-  }
+  };
+  onApplyChanges = () => {
+    localStorage.setItem("code", this.state.code);
+  };
   render() {
-    const {code, defaultValue} = this.state;
+    const { code, defaultValue } = this.state;
     const options = {
-      selectOnLineNumbers: true
+      selectOnLineNumbers: true,
     };
     return (
       <>
-      <MonacoEditor
-        width="1200"
-        height="600"
-        language="typescript"
-        theme="vs-dark"
-        defaultValue='//type your code here'
-        value={code}
-        options={options}
-        onChange={this.onChange}
-        editorDidMount={this.editorDidMount}
-      />
-      {/* <button onClick={this.onSubmit}>Check here</button> */}
+        <MonacoEditor
+          width="1200"
+          height="600"
+          language="typescript"
+          theme="vs-dark"
+          defaultValue="//type your code here"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+        />
+        <button className="btn" onClick={this.onApplyChanges}>
+          Apply changes
+        </button>
       </>
     );
   }
